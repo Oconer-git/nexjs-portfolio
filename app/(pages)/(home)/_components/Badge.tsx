90589b5dee49b5b9c02716b5f42d36f0ded84b1d
Donell Oconer <donellpie@gmail.com>
2025-01-31 11:23:52 +0800

    fixed badge padding

--- app/(pages)/(home)/_components/Badge.tsx
+++ app/(pages)/(home)/_components/Badge.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const Badge = ({ children }: PropsWithChildren) => {
     return (
-        <div className="rounded-[0.4rem] bg-primary px-[0.5rem] py-[0.1rem] text-[0.8rem] text-gray-200 sm:text-[1.1rem]">
+        <div className="rounded-[0.4rem] bg-primary px-[0.5rem] pb-[0.1rem] pt-[0.3rem] text-[0.8rem] text-gray-200 sm:text-[1.1rem]">
             {children}
         </div>
     );
