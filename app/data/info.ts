969e9129bef5287551bcfef79ef75a2c53e17675
Donell Oconer <donellpie@gmail.com>
2025-02-01 11:50:25 +0800

    changed resumelink

--- app/data/info.ts
+++ app/data/info.ts
@@ -1,5 +1,5 @@
 export const resumeLink =
-    'https://drive.google.com/file/d/1FaF06O2y26AmKccpAz0l-30IirNX4KX5/view?usp=sharing';
+    'https://drive.google.com/file/d/1sab7ocJRBa4f1WkkiGIvd-hpYGRSXZ5x/view?usp=sharing';
 
 export const email = 'oconerdoncarl@gmail.com';
 
