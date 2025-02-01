3deff6c7d969663db38a18731d1d95c8cd96ba8f
Donell Oconer <donellpie@gmail.com>
2025-02-01 11:45:47 +0800

    changed email

--- app/data/info.ts
+++ app/data/info.ts
@@ -1,7 +1,7 @@
 export const resumeLink =
     'https://drive.google.com/file/d/1FaF06O2y26AmKccpAz0l-30IirNX4KX5/view?usp=sharing';
 
-export const email = 'oconerdonell@gmail.com';
+export const email = 'oconerdoncarl@gmail.com';
 
 export const cpNumber = '0945  183  0225';
 
